688ea6786beed9e7308f6b767c9a5af665b987e3
edvardgospel <edvard.eros@yahoo.com>
2022-04-15 16:23:46 +0300

    Search Users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,27 +14,31 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  // const fetchUsers = async () => {
-  //   setLoading()
-  //   const response = await fetch(`${GITHUB_URL}/users`, {
-  //     headers: {
-  //       Authorization: `token ${GITHUB_TOKEN}`
-  //     }
-  //   })
-  //   const data = await response.json()
-  //   dispatch({
-  //     type: 'GET_USERS',
-  //     payload: data
+  const searchUsers = async (text) => {
+    setLoading()
 
-  //   })
-  // }
+    const params = new URLSearchParams({
+      q: text
+    })
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+    const { items } = await response.json()
+    dispatch({
+      type: 'GET_USERS',
+      payload: items
+
+    })
+  }
 
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
   return <GithubContext.Provider value={{
     users: state.users,
     loading: state.loading,
-    //fetchUsers
+    searchUsers
   }}>
     {children}
   </GithubContext.Provider>
